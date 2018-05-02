70c630ef1044b7bc56518cba9a7dac1af2f01a5d
Y2P <yigit.pilavci01@gmail.com>
2018-05-02 13:39:14 +0200

    Few Changes

--- encoder/encoder.cc
+++ encoder/encoder.cc
@@ -5,7 +5,7 @@
 #include <ignition/math/Vector3.hh>
 #include <boost/algorithm/string/replace.hpp>
 #include <string>
-
+// #include <ros/ros.h>
 // This plugin simulates the encoder sensor that measures the rotational
 // speed of the wheels of differential drive robot. It basically reads 
 // rotational velocities in the joints in the wheel per 50ms(20Hz) and 
@@ -14,6 +14,8 @@ namespace gazebo
 {
 	class Encoder : public ModelPlugin
 	{
+		//private: ros::NodeHandlePtr nh;
+		//private: ros::Timer timer;
 		private: physics::ModelPtr model;
 		private: event::ConnectionPtr updateConnection;
 		private: gazebo::physics::JointPtr joint_left;
@@ -25,14 +27,15 @@ namespace gazebo
 			// Get joints by name
   			this->joint_left = model->GetJoint(("left_wheel_hinge"));
   			this->joint_right = model->GetJoint(("right_wheel_hinge"));
-			
+			//this->nh = boost::make_shared<ros::NodeHandle>();
+        	//this->timer = nh->createTimer(ros::Duration(0.05), timerCallback);
 			this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&Encoder::OnUpdate, this));
 		}
 		public: void OnUpdate()
 		{	
 			// Print velocities
-			printf("Right Velocity: %.2f\n",this->GetRightVelocity());
-			printf("Left Velocity: %.2f\n",this->GetLeftVelocity());
+			printf("Right Velocity: %.3f ",this->GetRightVelocity());
+			printf("Left Velocity: %.3f\n",this->GetLeftVelocity());
 		}
 
 		// Get Velocities in Radian
