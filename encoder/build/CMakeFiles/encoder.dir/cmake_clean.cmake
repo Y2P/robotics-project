dde7f5822ada1fdc5551e710f6e61e6cc740c295
Y2P <yigit.pilavci01@gmail.com>
2018-05-02 02:46:16 +0200

    Encoder Plugin

---
+++ encoder/build/CMakeFiles/encoder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encoder.dir/encoder.cc.o"
+  "libencoder.pdb"
+  "libencoder.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ encoder/encoder.cc
@@ -0,0 +1,49 @@
+#include <functional>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+#include <boost/algorithm/string/replace.hpp>
+#include <string>
+
+// This plugin simulates the encoder sensor that measures the rotational
+// speed of the wheels of differential drive robot. It basically reads 
+// rotational velocities in the joints in the wheel per 50ms(20Hz) and 
+// prints out change which corresponds to speed
+namespace gazebo
+{
+	class Encoder : public ModelPlugin
+	{
+		private: physics::ModelPtr model;
+		private: event::ConnectionPtr updateConnection;
+		private: gazebo::physics::JointPtr joint_left;
+		private: gazebo::physics::JointPtr joint_right;
+
+		public: void Load(physics::ModelPtr model, sdf::ElementPtr sdf)
+		{
+			this->model = model;
+			// Get joints by name
+  			this->joint_left = model->GetJoint(("left_wheel_hinge"));
+  			this->joint_right = model->GetJoint(("right_wheel_hinge"));
+			
+			this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&Encoder::OnUpdate, this));
+		}
+		public: void OnUpdate()
+		{	
+			// Print velocities
+			printf("Right Velocity: %.2f\n",this->GetRightVelocity());
+			printf("Left Velocity: %.2f\n",this->GetLeftVelocity());
+		}
+
+		// Get Velocities in Radian
+		public: double GetLeftVelocity()
+		{
+			return this->joint_left->GetVelocity(0);
+		}
+		public: double GetRightVelocity()
+		{
+			return this->joint_right->GetVelocity(0);
+		}
+	};
+	GZ_REGISTER_MODEL_PLUGIN(Encoder)
+}
