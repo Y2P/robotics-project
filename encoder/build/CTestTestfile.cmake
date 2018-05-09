de8ef458fd2c1e31ebbaf9b6c53cb03dc0f4e6d5
Y2P <yigit.pilavci01@gmail.com>
2018-05-09 15:30:44 +0200

    LibCompiled

---
+++ encoder/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/y2p/Desktop/Robotics/Plugins/encoder
+# Build directory: /home/y2p/Desktop/Robotics/Plugins/encoder/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
